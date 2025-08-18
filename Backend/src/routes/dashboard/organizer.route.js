c39e4add88a18c66fcf0134e99a0b059ee3d7fdb
Kousik Mondal <mondalkousik2003@gmail.com>
2025-08-18 15:47:22 +0530

    edit flow upated

--- Backend/src/routes/dashboard/organizer.route.js
+++ Backend/src/routes/dashboard/organizer.route.js
@@ -5,7 +5,8 @@ import {
   viewUpcomingEvents,
   viewPastEvents,
   deleteEvent,
-  eventStatus
+  eventStatus,
+  viewToEditEvents
 } from "../../controllers/dashboard/organizer.controller.js";
 import { verifyJWT } from "../../middlewares/auth.middleware.js";
 import { restrictTo } from "../../middlewares/role.middleware.js";
@@ -20,5 +21,6 @@ router.get("/events/upcoming",verifyJWT,restrictTo("organizer"),viewUpcomingEven
 router.get("/events/past",verifyJWT,restrictTo("organizer"),viewPastEvents);
 router.delete("/events/:eventId",verifyJWT,restrictTo("organizer"),deleteEvent);
 router.get("/events/event-status",verifyJWT,restrictTo("organizer"),eventStatus);
+router.get("/events/pending-Edit",verifyJWT,restrictTo("organizer"),viewToEditEvents);
 
 export default router;
